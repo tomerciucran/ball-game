a0648d89f0eb5300bd6113bb0f48f6d50b62c892
Tomer Ciucran <tomercukran@gmail.com>
2016-10-10 16:48:16 +0200

    working on a new prototype

--- ballgame/GameScene.swift
+++ ballgame/GameScene.swift
@@ -11,46 +11,55 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var ball = SKSpriteNode(imageNamed: "Ball")
+    var ball = SKShapeNode(circleOfRadius: 20.0)
     
     override func didMove(to view: SKView) {
-        var ropeSegments = [SKSpriteNode]()
         
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
+        
+//        var ropeSegments = [SKSpriteNode]()
+//        
         let ropeHolder = SKSpriteNode(imageNamed: "RopeHolder")
         ropeHolder.position = CGPoint(x: frame.midX, y: frame.midY)
-        ropeSegments.append(ropeHolder)
+//        ropeSegments.append(ropeHolder)
         addChild(ropeHolder)
         
         ropeHolder.physicsBody = SKPhysicsBody(circleOfRadius: ropeHolder.size.width / 2)
         ropeHolder.physicsBody?.isDynamic = false
+//
+//        for i in 0..<30 {
+//            let ropeSegment = SKSpriteNode(imageNamed: "RopeTexture")
+//            let offset = ropeSegment.size.height * CGFloat(i + 1)
+//            ropeSegment.position = CGPoint(x: ropeHolder.position.x, y: ropeHolder.position.y - offset)
+//            ropeSegments.append(ropeSegment)
+//            addChild(ropeSegment)
+//            
+//            ropeSegment.physicsBody = SKPhysicsBody(rectangleOf: ropeSegment.size)
+//        }
+//        
+//        for i in 1..<31 {
+//            let nodeA = ropeSegments[i - 1]
+//            let nodeB = ropeSegments[i]
+//            let joint = SKPhysicsJointPin.joint(withBodyA: nodeA.physicsBody!, bodyB: nodeB.physicsBody!, anchor: CGPoint(x: nodeA.frame.midX, y: nodeA.frame.midY))
+//            
+//            physicsWorld.add(joint)
+//        }
         
-        for i in 0..<30 {
-            let ropeSegment = SKSpriteNode(imageNamed: "RopeTexture")
-            let offset = ropeSegment.size.height * CGFloat(i + 1)
-            ropeSegment.position = CGPoint(x: ropeHolder.position.x, y: ropeHolder.position.y - offset)
-            ropeSegments.append(ropeSegment)
-            addChild(ropeSegment)
-            
-            ropeSegment.physicsBody = SKPhysicsBody(rectangleOf: ropeSegment.size)
-        }
-        
-        for i in 1..<31 {
-            let nodeA = ropeSegments[i - 1]
-            let nodeB = ropeSegments[i]
-            let joint = SKPhysicsJointPin.joint(withBodyA: nodeA.physicsBody!, bodyB: nodeB.physicsBody!, anchor: CGPoint(x: nodeA.frame.midX, y: nodeA.frame.midY))
-            
-            physicsWorld.add(joint)
-        }
-        
-        ball.position = CGPoint(x: frame.midX, y: ropeSegments.last!.position.y - ball.size.height / 2)
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 20.0)
+        ball.fillColor = UIColor.red
         addChild(ball)
         
-        let joint = SKPhysicsJointPin.joint(withBodyA: ropeSegments.last!.physicsBody!, bodyB: ball.physicsBody!, anchor: CGPoint(x: ropeSegments.last!.frame.midX, y: ropeSegments.last!.frame.midY))
+//        let joint = SKPhysicsJointPin.joint(withBodyA: ropeSegments.last!.physicsBody!, bodyB: ball.physicsBody!, anchor: CGPoint(x: ropeSegments.last!.frame.midX, y: ropeSegments.last!.frame.midY))
+//        physicsWorld.add(joint)
+        
+        let joint = SKPhysicsJointSpring.joint(withBodyA: ropeHolder.physicsBody!, bodyB: ball.physicsBody!, anchorA: CGPoint(x: ropeHolder.frame.midX, y: ropeHolder.frame.midY), anchorB: CGPoint(x: ball.frame.midX, y: ball.frame.midY))
+        joint.damping = 0.2
+        joint.frequency = 0.5
         physicsWorld.add(joint)
         
-        let moveAction = SKAction.move(to: CGPoint(x: frame.midX - 100, y: frame.midY), duration: 0.5)
-        ball.run(moveAction)
+//        let moveAction = SKAction.move(to: CGPoint(x: frame.midX - 100, y: frame.midY), duration: 0.5)
+//        ball.run(moveAction)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
